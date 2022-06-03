37cdfefc5ca647085115966d87807c6ee4356d8c
qlaive <tdsfee@gmail.com>
2022-06-03 00:15:57 -0400

    Add project files.

---
+++ MVCWebAPP/Controllers/HelloWorldController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVCWebAPP.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ MVCWebAPP/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MVCWebAPP.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
