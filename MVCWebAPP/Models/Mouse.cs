a5e5c97e52162da3b8fc94d27ece3c3705feb9f1
qlaive <tdsfee@gmail.com>
2022-06-07 15:19:25 -0400

    Db Wired and created mouse class.

---
+++ MVCWebAPP/Models/Mouse.cs
@@ -0,0 +1,22 @@
+﻿namespace MVCWebAPP.Models
+{
+    public class Mouse
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Shape { get; set; }
+
+        public int Weight { get; set; }
+
+        public bool IsWireless { get; set; }
+
+        public int? Rank { get; set; }
+
+        public string Size { get; set; }
+
+        public string? URL { get; set; }
+
+    }
+}
